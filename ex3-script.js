f99a6f537e39e76bd6703f9da31b203814d97f10
fili0727 <filippa.arildsen@gmail.com>
2023-02-21 11:35:32 +0100

    update

--- ex3-script.js
+++ ex3-script.js
@@ -5,17 +5,14 @@ window.addEventListener("load", start);
 function start() {
   console.log("start");
   document.querySelector("#orange_container").addEventListener("click", jump);
-}
-function move() {
   document.querySelector("#orange_container").classList.add("move");
 }
-move();
+
+function jump() {
+  console.log("jump");
+  document.querySelector("#orange_container").classList.add("jump");
+}
 
 function paused() {
   document.querySelector("#orange_container").classList.add("paused");
-}
-function jump() {
-  console.log("jump");
-  paused();
-  document.querySelector("#orange_container").classList.add("jump");
 }
